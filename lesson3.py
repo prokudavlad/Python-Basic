f31a210e1926bd3a9634adfb0040193ec3d6e0ea
prokudavlad <37536024+prokudavlad@users.noreply.github.com>
2022-12-12 16:42:48 +0200

    Add files via upload

--- lesson3.py
+++ lesson3.py
@@ -1,12 +1,6 @@
-print("Hi, world!")
-
-# print('Hello, world!'*4)
-print('Hello, world!'+'Hello, world!'+'Hello, world!')
-print('Hello, world!'/'Hello, world!')
-
 # print('Hello Worlds')
 # print('Hello Worlds'*"er")
-print('Hello, world!'+'Hello, world!'+'Hello, world!')
+
 
 # # =============================================
 # # Соблюдение РЕР8. Неправильние отступи
@@ -17,14 +11,14 @@ print('Hello, world!'+'Hello, world!'+'Hello, world!')
 
 # =============================================
 # Зарезервированние слова. Сдежебние слова!
-int_ = 10
-print('int_: ', int_)
-string_number = '123'
-print('string_number: ', string_number)
-print('type(string_number): ', type(string_number))
-int_string_number = int(string_number)
-print('int_string_number: ', int_string_number)
-print(type(int_string_number))
+# int_ = 10
+# print('int_: ', int_)
+# string_number = '123'
+# print('string_number: ', string_number)
+# print('type(string_number): ', type(string_number))
+# int_string_number = int(string_number)
+# print('int_string_number: ', int_string_number)
+# print(type(int_string_number))
 # Исправление
 my_int = 10
 print('my_int: ', my_int)
@@ -33,14 +27,14 @@ nsrt = int(string_number)
 print('nsrt: ', nsrt)
 print('type(nsrt): ', type(nsrt))
 
-names = ['ann', 'sdsdf', 'awe']
-for name in names:
-    print('name', name)
+# names = ['ann', 'sdsdf', 'awe']
+# for name in names:
+#     print('name', name)
 
-names_age = {'ann': 10, 'sdsdf': 23, 'awe': 34}
-for name, age in names_age.items():
-    print('age: ', age)
-    print('name: ', name)
+# names_age = {'ann': 10, 'sdsdf': 23, 'awe': 34}
+# for name, age in names_age.items():
+#     print('age: ', age)
+#     print('name: ', name)
 
 # =============================================
 # # Регистрочувствительний
@@ -56,10 +50,10 @@ for name, age in names_age.items():
 # list_[2] = 56
 # print('list_: ', list_)
 
-# tuple_ = (1, 2, 3, 4, 5)
-# print('tuple_: ', tuple_)
-# tuple_[2] = 56
-# print('tuple_: ', tuple_)
+tuple_ = (1, 2, 3, 4, 5)
+print('tuple_: ', tuple_)
+tuple_[2] = 56
+print('tuple_: ', tuple_)
 
 # # =============================================
 # # Определение типа переменной. Функция type()
@@ -70,15 +64,19 @@ for name, age in names_age.items():
 
 # # =============================================
 # # Определение типов (int, float, str)
-# my_int = 15
-# print('my_int: ', my_int)
-# print('type(my_int): ', type(my_int))
-# my_float = 15.
-# print('my_float: ', my_float)
-# print('type(my_float): ', type(my_float))
-# my_str = '15'
-# print('my_str: ', my_str)
-# print('type(my_str): ', type(my_str))
+my_int = 15
+print('my_int: ', my_int)
+print('type(my_int): ', type(my_int))
+my_float = 15.
+print('my_float: ', my_float)
+print('type(my_float): ', type(my_float))
+my_str = '15'
+print('my_str: ', my_str)
+print('type(my_str): ', type(my_str))
+
+# NT = None
+# print('NT: ', NT)
+# print('type(NT): ', type(NT))
 
 # # =============================================
 # # Введение значений переменнх с клавиатури
@@ -87,6 +85,7 @@ for name, age in names_age.items():
 # print('type(my_value): ', type(my_value))
 # # # функция input всегда возвращает строку
 # my_value = my_value*2
+# print('my_value: ', my_value)
 # my_value = my_value + my_value
 # print('my_value: ', my_value)
 # print('type(my_value): ', type(my_value))
@@ -105,12 +104,14 @@ for name, age in names_age.items():
 # print('my_int: ', my_int)
 # print('type(my_int): ', type(my_int))
 
+
 # my_str = '10.0'
 # print('my_str: ', my_str)
 # print('type(my_str): ', type(my_str))
-# print('int(my_str): ', int(my_str))
-# print('float(my_str): ', float(my_str))
-# print('type(float(my_str)): ', type(float(my_str)))
+# # print('int(my_str): ', int(my_str))
+# float_my_str = float(my_str)
+# print('float_my_str): ', float_my_str)
+# print('type(float_my_str): ', type(float_my_str))
 
 # my_int = 10
 # print('my_int: ', my_int)
@@ -146,17 +147,22 @@ for name, age in names_age.items():
 
 # my_str = 'ten'
 # print('int(my_str: ', int(my_str))
+
 # my_float = 100.
 # print('my_float: ', my_float)
 # print('type(my_float): ', type(my_float))
 
 # =============================================
 # Булевий тип данних
-# my_bool = True
+my_bool = True
+print('my_bool: ', my_bool)
+print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2==2
 # print('my_bool: ', my_bool)
 # print('type(my_bool): ', type(my_bool))
 
-# my_bool = 2==2
+# my_bool = 2==2.0
 # print('my_bool: ', my_bool)
 # print('type(my_bool): ', type(my_bool))
 
@@ -209,9 +215,16 @@ for name, age in names_age.items():
 # print('bool(my_int): ', bool(my_int))
 # print('type(my_int): ', type(my_int))
 
-# list_ = [1, 2,3,4,5]
+# list_ = [1, 2, 3, 4, 5]
 # print('len(list_): ', len(list_))
 # if len(list_):
+#     print('список имеет значения')
+# else:
+#     print('список не имеет значения')
+
+# list_ = []
+# print('len(list_): ', len(list_))
+# if list_:
 #     print('список имеет значения')
 # else:
 #     print('список не имеет значения')
@@ -226,15 +239,15 @@ for name, age in names_age.items():
 # print('type(my_int): ', type(my_int))
 
 # Зміна точності
-# my_int = 0.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
-# print('bool(my_int): ', bool(my_int))
-# print('type(my_int): ', type(my_int))
-
+my_int = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
+print('bool(my_int): ', bool(my_int))
+print('type(my_int): ', type(my_int))
+print(len('00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'))
 
 # val_1 = 24.3524634576
 # val_2 = 24.3524634575
 # print(val_1==val_2)
-# print(abs(val_1 - val_2) < 0.001)
+# print(abs(val_1 - val_2) < 0.00001)
 
 # my_str = '1'
 # print('bool(my_str): ', bool(my_str))
@@ -251,6 +264,8 @@ for name, age in names_age.items():
 # print('bool(my_none): ', bool(my_none))
 
 # my_str = 'True'
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
 # print('bool(my_str): ', bool(my_str))
 # my_bool = True
 # print('my_bool: ', my_bool)
